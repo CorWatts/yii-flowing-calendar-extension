a301d1eec1ae1aa895361f21d572440ef25b18f4
Corey Watts <corwatts@gmail.com>
2012-04-27 22:48:41 -0700

    Fixed bug with previous/next month links not going to correct controller/action.

--- flowing-calendar/views/flowingCalendar.php
+++ flowing-calendar/views/flowingCalendar.php
@@ -1,7 +1,7 @@
 <h2 style="text-align:center;"><?php echo $this->title; ?></h2>
 <?php 
-$this->storePreviousLink = CHtml::link("<< Previous Month", array('site/index',	'month'=>$this->previousMonth, 'year'=>$this->yearPreviousMonth));
-$this->storeNextLink = CHtml::link("Next Month >>", array('site/index',	'month'=>$this->nextMonth, 'year'=>$this->yearNextMonth));
+$this->storePreviousLink = CHtml::link("<< Previous Month", array(Yii::app()->controller->id."/".Yii::app()->controller->action->id,	'month'=>$this->previousMonth, 'year'=>$this->yearPreviousMonth));
+$this->storeNextLink = CHtml::link("Next Month >>", array(Yii::app()->controller->id."/".Yii::app()->controller->action->id, 'month'=>$this->nextMonth, 'year'=>$this->yearNextMonth));
 echo $this->printStartForm();
 echo $this->storePreviousLink;
 echo ".::";
